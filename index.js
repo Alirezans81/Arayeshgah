c21c020673a4a0d151fd24ff8df0b91a1f9f9dc5
Alireza <alireza.nasiri.381@gmial.com>
2022-08-31 01:04:18 +0430

    fixing resending respond

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const router = require('./src/routes');
+const router = require("./src/routes");
 
-require('./startup/config')(app, express);
-require('./startup/db')();
-require('./startup/logging')();
+require("./startup/config")(app, express);
+require("./startup/db")();
+require("./startup/logging")();
 
-app.use('/api', router);
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    data: null,
+    message: "app works!",
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,25 +1,26 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 async function isLoggined(req, res, next) {
-  const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
 
-  if (!token) res.status(401).send('access denied');
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
-    const user = await User.findById(decoded.id);
-    console.log(user);
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(400).send('invalid token');
+  if (!token) res.status(401).send("access denied");
+  else {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
+      const user = await User.findById(decoded.id);
+      console.log(user);
+      req.user = user;
+      next();
+    } catch (error) {
+      res.status(400).send("invalid token");
+    }
   }
 }
 
 async function isAdmin(req, res, next) {
-  if (!req.user.isAdmin) res.status(403).send('access denied');
-  next();
+  if (!req.user.isAdmin) res.status(403).send("access denied");
+  else next();
 }
 
-module.exports = {isLoggined, isAdmin};
+module.exports = { isLoggined, isAdmin };
